b6517fb274738fe0ab754f8443dbcf234352f80f
jubobs <jubobs-oss@protonmail.com>
2021-05-04 12:40:51 +0200

    Fix bug in bimap

--- src/bimap/bimap/bimap.go
+++ src/bimap/bimap/bimap.go
@@ -6,14 +6,14 @@ type Bimap struct {
 	Forward, Inverse map[string]string
 }
 
-func New() Bimap {
-	return Bimap{
+func New() *Bimap {
+	return &Bimap{
 		Forward: make(map[string]string),
 		Inverse: make(map[string]string),
 	}
 }
 
-func (bi Bimap) Store(key string, value string) {
+func (bi *Bimap) Store(key string, value string) {
 	k, exists := bi.Inverse[value]
 	if exists { // value is already associated with k
 		delete(bi.Forward, k)
@@ -26,16 +26,16 @@ func (bi Bimap) Store(key string, value string) {
 	bi.Inverse[value] = key
 }
 
-func (bi Bimap) LookupValue(key string) (string, bool) {
+func (bi *Bimap) LookupValue(key string) (string, bool) {
 	v, ok := bi.Forward[key]
 	return v, ok
 }
 
-func (bi Bimap) LookupKey(value string) (string, bool) {
+func (bi *Bimap) LookupKey(value string) (string, bool) {
 	k, ok := bi.Inverse[value]
 	return k, ok
 }
 
-func (bi Bimap) String() string {
+func (bi *Bimap) String() string {
 	return fmt.Sprintf("bi%v", bi.Forward)
 }
