60224c4dc3d1b787fbe7566cb5bac49f420597d9
David Aparicio <david.aparicio@ovhcloud.com>
2022-05-11 17:45:02 +0200

    ✨ feat: using the HTTP request context + Counting semaphore pattern implementation

--- src/namecheck/generate_doc.sh
+++ src/namecheck/generate_doc.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+#present main.slide
+#open "http://127.0.0.1:3999"
 go test -v ./twitter
+#mkdir -p $HOME/go/{bin,src,pkg}
 #go get golang.org/x/tools/cmd/godoc
+#go install golang.org/x/tools/cmd/present && go install golang.org/x/tools/cmd/godoc
 godoc -http=:6060 -play #-goroot=$HOME/go
 open "http://127.0.0.1:6060"
