3d48fd76fe8b9525ebcd16ead75c48f67ae36bcd
David Aparicio <david.aparicio@ovhcloud.com>
2022-05-12 15:51:29 +0200

    ✨ feat: Reducing tail latency with hedged requests (2 solutions)

--- src/ovhreq2/main.go
+++ src/ovhreq2/main.go
@@ -15,35 +15,24 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-func get(ctx context.Context, results chan Result, dc string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	res, err := fetchFrom(ctx, dc)
-	if err != nil {
-		return
-	}
-	results <- res
-}
-
 func main() {
-	//dc := "Strasbourg"
-	dcs := []string{"Strasbourg", "Roubaix", "Paris", "Sydney", "Beauharnois"}
-	results := make(chan Result)
-
-	//rand.Seed(time.Now().UnixNano())
-	//rand.Shuffle(len(dcs), func(i, j int) { dcs[i], dcs[j] = dcs[j], dcs[i] })
-
+	dcs := []string{"Strasbourg", "Roubaix", "Paris"}
+	results := make(chan Result, len(dcs)-1)
 	ctx, cancel := context.WithTimeout(context.Background(), 700*time.Millisecond)
 	defer cancel()
-
 	var wg sync.WaitGroup
 	defer wg.Wait()
-	//wg.Add(len(dcs))
-
 	for _, dc := range dcs {
 		dc := dc
 		wg.Add(1)
-		go get(ctx, results, dc, &wg)
-
+		go func() {
+			defer wg.Done()
+			res, err := fetchFrom(ctx, dc)
+			if err != nil {
+				return
+			}
+			results <- res
+		}()
 		select {
 		case <-ctx.Done():
 			fmt.Fprintln(os.Stderr, ctx.Err())
@@ -56,6 +45,7 @@ func main() {
 			continue
 		}
 	}
+
 }
 
 func fetchFrom(ctx context.Context, dc string) (Result, error) {
@@ -71,3 +61,4 @@ func fetchFrom(ctx context.Context, dc string) (Result, error) {
 	res := Result(fmt.Sprintf("data from %s", dc))
 	return res, nil
 }
+
