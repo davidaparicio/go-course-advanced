3253b18d10c117dc7335fb973d4d89a1a5c25d22
David Aparicio <david.aparicio@ovhcloud.com>
2022-05-13 17:43:38 +0200

    ✨ feat: patch msg=script security flaw (amend)

--- src/sniffing/main.go
+++ src/sniffing/main.go
@@ -14,7 +14,7 @@ func main() {
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	// http://127.0.0.1:8080/?msg=script>window.alert("sometext");</script>
+	// http://127.0.0.1:8080/?msg=<script>window.alert("sometext");</script>
 	// Without => Content-Type: text/html; charset=utf-8
 	// The patch
 	w.Header().Set("Content-Type", "text/plain")
