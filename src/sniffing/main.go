490a78d4800e1bb0618b44dfc878c64ebaba4296
David Aparicio <david.aparicio@ovhcloud.com>
2022-05-13 16:24:43 +0200

    ✨ feat: patch msg=script security flaw

--- src/sniffing/main.go
+++ src/sniffing/main.go
@@ -14,6 +14,10 @@ func main() {
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
+	// http://127.0.0.1:8080/?msg=script>window.alert("sometext");</script>
+	// Without => Content-Type: text/html; charset=utf-8
+	// The patch
+	w.Header().Set("Content-Type", "text/plain")
 	msg := r.URL.Query().Get("msg")
 	if msg == "" {
 		http.Error(w, "'msg' query param is required", http.StatusBadRequest)
