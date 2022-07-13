6ae6c35a997f3c98331e4b6fefe1572f55c75a5e
David Aparicio <david.aparicio@ovhcloud.com>
2022-07-13 14:52:36 +0200

    ✨ feat: memorylayout into SVG files

--- src/memorylayout/go.mod
+++ src/memorylayout/go.mod
@@ -2,4 +2,10 @@ module mymodulepath
 
 go 1.16
 
-require honnef.co/go/tools v0.3.1 // indirect
+require (
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
+	golang.org/x/tools v0.1.11 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	honnef.co/go/tools v0.3.2 // indirect
+)
--- src/memorylayout/see_memory.sh
+++ src/memorylayout/see_memory.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
 
-#go install honnef.co/go/tools/cmd/structlayout
-#go install honnef.co/go/tools/cmd/structlayout-pretty
-#go install honnef.co/go/tools/cmd/structlayout-optimize
-go get honnef.co/go/tools/cmd/structlayout
-go get honnef.co/go/tools/cmd/structlayout-pretty
-go get honnef.co/go/tools/cmd/structlayout-optimize
-echo "BEFORE MEMORY OPTIMIZATION"
-structlayout -json ./main.go User | structlayout-pretty | tee old.mem
-echo "AFTER MEMORY OPTIMIZATION"
-structlayout -json ./main.go User2 | structlayout-pretty | tee new.mem
+go get -u honnef.co/go/tools/cmd/structlayout
+go get -u honnef.co/go/tools/cmd/structlayout-pretty
+go get -u honnef.co/go/tools/cmd/structlayout-optimize
+go install honnef.co/go/tools/cmd/structlayout
+go install honnef.co/go/tools/cmd/structlayout-pretty
+go install honnef.co/go/tools/cmd/structlayout-optimize
 
-echo "=============================================================================="
-echo ""
-echo "=========================="
-echo "=========================="
-echo "== MEMORY OPTIMIZATION =="
-echo "=========================="
-echo "=========================="
+#echo "BEFORE MEMORY OPTIMIZATION"
+#structlayout -json ./main.go User | structlayout-pretty | tee old.mem
+#echo "AFTER MEMORY OPTIMIZATION"
+#structlayout -json ./main.go User2 | structlayout-pretty | tee new.mem
+
+go get -u github.com/ajstarks/svgo/structlayout-svg
+go install github.com/ajstarks/svgo/structlayout-svg
+
+echo "BEFORE MEMORY OPTIMIZATION"
+structlayout -json ./main.go User | structlayout-svg -t "User, before optimization" > user.svg
+echo "AFTER MEMORY OPTIMIZATION"
+structlayout -json ./main.go User2 | structlayout-svg -t "User, after optimization" > user2.svg
+
+#echo "=============================================================================="
+#echo ""
+#echo "=========================="
+#echo "=========================="
+#echo "== MEMORY OPTIMIZATION =="
+#echo "=========================="
+#echo "=========================="
 #https://difftastic.wilfred.me.uk/installation.html
 #brew install difftastic
-difft old.mem new.mem
+#difft old.mem new.mem
+
+open user.svg
+open user2.svg
