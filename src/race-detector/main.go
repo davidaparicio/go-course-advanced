6ff2df077155ca2ec9034c520317410654526698
David Aparicio <david.aparicio@free.fr>
2022-12-11 11:32:11 +0100

    🐛 fix: race with locks

--- src/race-detector/main.go
+++ src/race-detector/main.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
 var (
 	counter int64
+	mux     *sync.RWMutex
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	mux.Lock()
 	counter++
+	mux.Unlock()
+	mux.RLock()
 	fmt.Fprintln(w, counter)
+	mux.RUnlock()
 }
 
 func main() {
+	mux = &sync.RWMutex{}
 	http.HandleFunc("/", handler)
 	addr := ":8080"
 	log.Printf("server starting on %s", addr)
